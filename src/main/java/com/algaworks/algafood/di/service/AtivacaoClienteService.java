3b7cfaea5d1fcbdeac0b3ed99d91137d0d1cd8df
Jackson <pertusatti22@gmail.com>
2022-10-01 19:47:37 -0300

    Exemplificando aplicação de AutoWired nos construtores de AtivacaoClienteService

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -3,15 +3,24 @@ package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
-    final private Notificador notificador;
+    private Notificador notificador;
 
+    // onde estamos injetando o notificador? no construtor, então ele é um ponto de injeção.
+    @Autowired
     public AtivacaoClienteService(Notificador notificador) {
         this.notificador = notificador;
+    }
+
+    // Quando temos dois construtores, o Autowired indica para o spring qual deve ser usado
+    // no momento que a aplicação está sendo iniciada
+    public AtivacaoClienteService(String qualquer) {
+
     }
 
     public void ativar(Cliente cliente) {
