906833f8b8e0f7f20f3a1d5c09b3df6ed35cab66
Jackson <pertusatti22@gmail.com>
2022-10-01 18:10:52 -0300

    Transformando NotificadorEmail em Bean e mostrando seu resultado no Start do projeto

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.stereotype.Component;
 
 
+@Component // uma das formas de transformar a classe em um bean para ser gerenciado pelo spring "bean spring"
 public class NotificadorEmail{
+
+    public NotificadorEmail(){
+        System.out.println("Construtor chamado");// Por ser um componente, ele já é instanciado no play da app
+    }
 
     public void notificar(Cliente cliente, String mensagem) {
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
