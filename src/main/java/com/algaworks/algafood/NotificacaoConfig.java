38d735d8201dce58b9e1a26903dea9bbcb646b64
Jackson <pertusatti22@gmail.com>
2022-10-01 19:31:55 -0300

    Separando os Beans em arquivos diferentes

---
+++ src/main/java/com/algaworks/algafood/NotificacaoConfig.java
@@ -0,0 +1,18 @@
+package com.algaworks.algafood;
+
+import com.algaworks.algafood.di.notificacao.NotificadorEmail;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NotificacaoConfig {
+
+    @Bean
+    public NotificadorEmail notificadorEmail() {
+        NotificadorEmail notificador = new NotificadorEmail("smtp.algamail.com.br");
+        notificador.setCaixaAlta(true);
+
+        return notificador;
+    }
+
+}
