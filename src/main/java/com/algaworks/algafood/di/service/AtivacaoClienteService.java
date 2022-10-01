1ae83f0ad71e80d605bdf9264da96ef4f9e84826
Jackson <pertusatti22@gmail.com>
2022-10-01 19:37:16 -0300

    Retornando classes para component e removendo as classes de config e limpando prints no console

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -3,15 +3,15 @@ package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
     final private Notificador notificador;
 
     public AtivacaoClienteService(Notificador notificador) {
         this.notificador = notificador;
-
-        System.out.println("AtivacaoClienteService " + notificador);
     }
 
     public void ativar(Cliente cliente) {
