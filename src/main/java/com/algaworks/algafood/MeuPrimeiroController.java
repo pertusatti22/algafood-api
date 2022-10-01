7c962d28e4f172b1e616cec109a32d55623ca65f
Jackson <pertusatti22@gmail.com>
2022-10-01 18:13:56 -0300

    Transformando AtivacaoClienteService em Bean e removendo comentários e entendendo que controller também é componente.

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller //Transforma a classe em uma controller para o Spring
+@Controller
 public class MeuPrimeiroController {
 
-    @GetMapping("/hello") //Indica o endpoint para acessar a controller no navegador
-    @ResponseBody //Indica que o body seja devolvido pro navegador no retorno
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "Hello!";
     }
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -3,7 +3,9 @@ package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.NotificadorEmail;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
     private NotificadorEmail notificador;
