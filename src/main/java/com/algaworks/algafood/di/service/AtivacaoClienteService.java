206ab9b09f739b6f24cf7e4b921ac09e49cee469
Jackson <pertusatti22@gmail.com>
2022-10-01 20:09:49 -0300

    Exemplificando aplicação de AutoWired no metodo Setter

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -11,21 +11,20 @@ public class AtivacaoClienteService {
 
     private Notificador notificador;
 
-    // onde estamos injetando o notificador? no construtor, então ele é um ponto de injeção.
-    @Autowired
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-    }
+//    @Autowired
+//    public AtivacaoClienteService(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 
-    // Quando temos dois construtores, o Autowired indica para o spring qual deve ser usado
-    // no momento que a aplicação está sendo iniciada
-    public AtivacaoClienteService(String qualquer) {
-
-    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    }
+
+    @Autowired // Ponto de injeção via setter
+    public void setNotificador(Notificador notificador) {
+        this.notificador = notificador;
     }
 }
