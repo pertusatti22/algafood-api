65331540b4d41c343969dbd5487dfbb75e83ef26
Jackson <pertusatti22@gmail.com>
2022-10-01 18:51:11 -0300

    Injetando AtivacaoClienteService em MeuPrimeiroController e modificando endpoint Hello

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +9,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MeuPrimeiroController {
 
+    final private AtivacaoClienteService ativacaoClienteService;
+
+    public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+        this.ativacaoClienteService = ativacaoClienteService;
+
+        System.out.println("MeuPrimeiroController: " + ativacaoClienteService);
+    }
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello(){
+        Cliente joao = new Cliente("João", "joao@gmail.com", "61 99999999");
+
+        ativacaoClienteService.ativar(joao);
+
         return "Hello!";
     }
 
