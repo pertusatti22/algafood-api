c7405e8d4aafb087f869ec2b7fa8976a65de51ba
Jackson <pertusatti22@gmail.com>
2022-10-01 23:15:25 -0300

    Desambiguação de beans com Qualifier em NotificadorSms e NotificadorEmail

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSms.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSms.java
@@ -1,10 +1,11 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-//@Primary
+
+@Qualifier("sms")
 @Component
 public class NotificadorSms implements Notificador {
 
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -4,6 +4,7 @@ package com.algaworks.algafood.di.service;
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,6 +12,8 @@ import java.util.List;
 @Component
 public class AtivacaoClienteService {
 
+    //@Qualifier("email")
+    @Qualifier("sms")
     @Autowired
     private Notificador notificador;
 
