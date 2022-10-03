2f9bf799c384b1843cf3fc7082d33751e7a1401e
Jackson <pertusatti22@gmail.com>
2022-10-03 13:26:23 -0300

    utilizando propriedades customizadas e acessando propriedades com ConfigurationProperties

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -12,15 +13,12 @@ import java.sql.SQLOutput;
 @Component
 public class NotificadorEmailMock implements Notificador {
 
-    @Value("${notificador.email.host-servidor}")
-    private String host;
-    @Value("${notificador.email.porta-servidor}")
-    private Integer porta;
-
+    @Autowired
+    private NotificadorProperties properties;
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.println("Host: " + host);
-        System.out.println("Porta: " + porta);
+        System.out.println("Host: " + properties.getHostServidor());
+        System.out.println("Porta: " + properties.getPortaServidor());
 
         System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
