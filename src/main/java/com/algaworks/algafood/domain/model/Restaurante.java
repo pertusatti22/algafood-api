7b3e47c3199edb73f9ee2284ef6455b1b6444fb4
Jackson <pertusatti22@gmail.com>
2022-10-10 11:42:51 -0300

    3.19. Propriedade nullable de @Column e @JoinColumn.3.18 teórica

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -17,10 +17,13 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
+    @Column(name= "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
     @ManyToOne
+    @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 }
