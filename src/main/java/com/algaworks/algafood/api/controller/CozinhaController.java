d93b273c907b577ee2c7d80c81503a205d2b7fa0
pertusatti22 <pertusatti22@gmail.com>
2022-10-21 16:10:40 -0300

    add 4.13. Implementando content negotiation para retornar JSON ou XML

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value ="/cozinhas") //utilizando aqui efetiva para todos os endpoints, produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
-    public List<Cozinha> listar() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listarJSON() {
+        System.out.println("Json");
         return cozinhaRepository.listar();
     }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Cozinha> listarXML() {
+        System.out.println("Xml");
+        return cozinhaRepository.listar();
+    }
+    //2 Mapeamentos para o mesmo endpoint, diferenciados pelo tipo de retorno, json ou xml
 }
