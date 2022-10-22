cf792cfc3a841b320a044c1d0b0d919875066ac9
pertusatti22 <pertusatti22@gmail.com>
2022-10-22 17:09:45 -0300

    add 4.21. Corrigindo o Status HTTP para resource inexistente

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,8 +4,6 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,18 +32,12 @@ public class CozinhaController {
 
     @GetMapping(value = "/{cozinhaId}")
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId){
-        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-        // return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-        // return ResponseEntity.ok(cozinha); versão shortcode
+            Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 
-        //Adicionando headers a resposta
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
-
-        return ResponseEntity
-                .status(HttpStatus.FOUND)
-                .headers(headers)
-                .build();
+            if(cozinha != null) {
+                return ResponseEntity.ok(cozinha);
+            }
+            return ResponseEntity.notFound().build();
     }
 
 }
