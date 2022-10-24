7278f81fc08c7ecf24306982e0c49ce41460b0ae
pertusatti22 <pertusatti22@gmail.com>
2022-10-24 12:52:00 -0300

    Refactor Desafio 4.34 correção de salvar CadastroCidadeService, para verificar se estado existe

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -3,7 +3,9 @@ package com.algaworks.algafood.domain.service;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -14,8 +16,21 @@ public class CadastroCidadeService {
 
     @Autowired
     private CidadeRepository cidadeRepository;
+    @Autowired
+    private EstadoRepository estadoRepository;
 
     public Cidade salvar(Cidade cidade) {
+
+        Long estadoId = cidade.getEstado().getId();
+        Estado estado = estadoRepository.buscar(estadoId);
+
+        if (estado == null) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe cadastro de estado com código %d", estadoId));
+        }
+
+        cidade.setEstado(estado);
+
         return cidadeRepository.salvar(cidade);
     }
 
