21f437acb951b309856777e35f57cdbed967d848
pertusatti22 <pertusatti22@gmail.com>
2023-04-18 14:31:09 -0300

    add 5.19. Injetando o próprio repositório na implementação customizada e a anotação @Lazy

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -13,9 +13,6 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
-import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
-
 @RestController
 @RequestMapping("/teste")
 public class TesteController {
@@ -73,9 +70,7 @@ public class TesteController {
 
     @GetMapping("/restaurantes/com-frete-gratis")
     public List<Restaurante> restaurantesComFreteGratis(String nome) {
-
-        return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
-
+        return restauranteRepository.findComFreteGratis(nome);
     }
 
 }
--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepositoryQueries.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepositoryQueries.java
@@ -6,5 +6,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface RestauranteRepositoryQueries {
-    public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+    List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+
+    List<Restaurante> findComFreteGratis(String nome);
 }
