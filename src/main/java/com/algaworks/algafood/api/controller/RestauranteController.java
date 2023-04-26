8a3723cbbb45526bc39f6d65ca880c4c133c47eb
pertusatti22 <pertusatti22@gmail.com>
2023-04-25 23:13:58 -0300

    add 6.11. Entendendo o Lazy Loading

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -30,7 +30,16 @@ public class RestauranteController {
 
     @GetMapping
     public List<Restaurante> listar() {
-        return restauranteRepository.findAll();
+
+        List<Restaurante> restaurantes = restauranteRepository.findAll();
+
+        System.out.println(restaurantes.get(0).getNome());
+        restaurantes.get(0).getFormasPagamento().forEach(System.out::println);
+
+        System.out.println(restaurantes.get(1).getNome());
+        restaurantes.get(1).getFormasPagamento().forEach(System.out::println);
+
+        return restaurantes;
     }
 
     @GetMapping("/{restauranteId}")
