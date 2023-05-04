f2a8c525c861f519e4fe53fed2369f437f23a1b9
pertusatti22 <pertusatti22@gmail.com>
2023-05-04 13:15:40 -0300

    add 7.12. Desafio: Criando migrações e mapeando as entidades Pedido e ItemPedido

---
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedido {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantidade;
+    private BigDecimal precoUnitario;
+    private BigDecimal precoTotal;
+    private String Observacao;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Produto produto;
+}
