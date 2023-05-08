de3ff0fddb34eebf94b369acb52adf9b3876d9f1
pertusatti22 <pertusatti22@gmail.com>
2023-05-08 16:57:11 -0300

    add 8.14. Desafio: implementando exception handler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import org.springframework.http.HttpStatus;
@@ -22,6 +23,18 @@ public class ApiExceptionHandler {
                 .build();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(problema);
+    }
+
+    @ExceptionHandler(EntidadeEmUsoException.class)
+    public ResponseEntity<?> tratarEntidadeEmUsoException(
+            EntidadeEmUsoException e) {
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(problema);
     }
 
--- src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,11 +1,7 @@
 package com.algaworks.algafood.domain.exception;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public abstract class EntidadeNaoEncontradaException extends RuntimeException {
+public abstract class EntidadeNaoEncontradaException extends NegocioException {
 
     private static final long serialVersionUID = 1L;
 
