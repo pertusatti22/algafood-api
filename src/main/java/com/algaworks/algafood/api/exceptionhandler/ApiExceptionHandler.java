47ebafc58550a0e6e8363669328dd1b6fd6e792b
pertusatti22 <pertusatti22@gmail.com>
2023-05-08 17:23:08 -0300

    add 8.16. Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,10 +3,12 @@ package com.algaworks.algafood.api.exceptionhandler;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -49,4 +51,24 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(problema);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        if (body == null) {
+            body = Problema.builder()
+                    .dataHora(LocalDateTime.now())
+                    .mensagem(status.getReasonPhrase())
+                    .build();
+        } else if (body instanceof String) {
+            body = Problema.builder()
+                    .dataHora(LocalDateTime.now())
+                    .mensagem((String) body)
+                    .build();
+        }
+
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
 }
