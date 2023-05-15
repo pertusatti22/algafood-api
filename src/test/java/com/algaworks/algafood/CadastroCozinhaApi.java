a5d3e643879cb9fb8f3c5f66eaab773d00f7855c
pertusatti22 <pertusatti22@gmail.com>
2023-05-15 14:41:29 -0300

    add 10.13. Configurando um banco de testes e usando @TestPropertySource

--- src/test/java/com/algaworks/algafood/CadastroCozinhaApi.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaApi.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
@@ -16,6 +17,7 @@ import static org.hamcrest.Matchers.hasSize;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CadastroCozinhaApi {
 
     @LocalServerPort
