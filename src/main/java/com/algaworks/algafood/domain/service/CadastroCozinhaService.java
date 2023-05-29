4fde45a64b89d3d842dc70fbaa3ddb070422350d
pertusatti22 <pertusatti22@gmail.com>
2023-05-29 20:11:23 -0300

    add 11.21. Corrigindo bug de tratamento de exception de integridade de dados com flush do JPA

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -29,6 +29,7 @@ public class CadastroCozinhaService {
     public void remover(Long cozinhaId) {
         try {
             cozinhaRepository.deleteById(cozinhaId);
+            cozinhaRepository.flush();
         } catch (EmptyResultDataAccessException e) {
             throw new CozinhaNaoEncontradaException(cozinhaId);
         } catch (DataIntegrityViolationException e) {
