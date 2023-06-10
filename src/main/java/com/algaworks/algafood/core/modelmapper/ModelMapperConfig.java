8c6b58f935959721e4589d9a2a990230c52af4f8
pertusatti22 <pertusatti22@gmail.com>
2023-06-10 18:43:40 -0300

    add 12.6. Adicionando endereço no modelo da representação do recurso de restaurante

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.core.modelmapper;
 
+import com.algaworks.algafood.api.model.EnderecoModel;
+import com.algaworks.algafood.domain.model.Endereco;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +12,16 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+        var modelMapper = new ModelMapper();
 
+        var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(
+                Endereco.class, EnderecoModel.class
+        );
+
+        enderecoToEnderecoModelTypeMap.<String>addMapping(
+                estado -> estado.getCidade().getEstado().getNome(),
+                (nome, value) -> nome.getCidade().setEstado(value));
+
+        return modelMapper;
+    }
 }
