633fe4db446fdcef4a20b5e75ac71ece48767bcc
pertusatti22 <pertusatti22@gmail.com>
2023-06-28 12:07:30 -0300

    add 12.15. Desafio: implementando os endpoints de associação de grupos com permissões

--- src/main/java/com/algaworks/algafood/domain/service/CadastroGrupoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroGrupoService.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.domain.service;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.GrupoNaoEncontradoException;
 import com.algaworks.algafood.domain.model.Grupo;
+import com.algaworks.algafood.domain.model.Permissao;
 import com.algaworks.algafood.domain.repository.GrupoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -18,6 +19,9 @@ public class CadastroGrupoService {
 
     @Autowired
     private GrupoRepository grupoRepository;
+
+    @Autowired
+    private CadastroPermissaoService cadastroPermissaoService;
 
     @Transactional
     public Grupo salvar(Grupo grupo) {
@@ -41,5 +45,21 @@ public class CadastroGrupoService {
     public Grupo encontrar(Long grupoId) {
         return grupoRepository.findById(grupoId)
                 .orElseThrow(() -> new GrupoNaoEncontradoException(grupoId));
+    }
+
+    @Transactional
+    public void desassociarPermissao(Long grupoId, Long permissaoId) {
+        Grupo grupo = encontrar(grupoId);
+        Permissao permissao = cadastroPermissaoService.encontrar(permissaoId);
+
+        grupo.removerPermissao(permissao);
+    }
+
+    @Transactional
+    public void associarPermissao(Long grupoId, Long permissaoId) {
+        Grupo grupo = encontrar(grupoId);
+        Permissao permissao = cadastroPermissaoService.encontrar(permissaoId);
+
+        grupo.adicionarPermissao(permissao);
     }
 }
