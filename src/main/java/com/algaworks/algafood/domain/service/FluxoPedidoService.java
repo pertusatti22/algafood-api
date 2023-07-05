7acf5320bd87b75f4cbbd84b8438341ced1ee9ea
pertusatti22 <pertusatti22@gmail.com>
2023-07-05 20:43:15 -0300

    add 12.22. Implementando endpoint de transição de status de pedidos

---
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.OffsetDateTime;
+
+@Service
+public class FluxoPedidoService {
+
+    @Autowired
+    private EmissaoPedidoService emissaoPedidoService;
+
+    @Transactional
+    public void confirmar(Long pedidoId) {
+        Pedido pedido = emissaoPedidoService.encontrar(pedidoId);
+
+        if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+            throw new NegocioException(String.format("Status do pedido %d não pode ser alterado de %s para %s",
+                    pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CONFIRMADO.getDescricao()));
+        }
+
+        pedido.setStatus(StatusPedido.CONFIRMADO);
+        pedido.setDataConfirmacao(OffsetDateTime.now());
+    }
+
+}
