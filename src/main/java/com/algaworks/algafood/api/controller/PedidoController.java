ce66ccf42d71274d2f93797cb0947fac5f3b339c
pertusatti22 <pertusatti22@gmail.com>
2023-07-06 09:05:50 -0300

    add 13.2. Limitando os campos retornados pela API com @JsonFilter do Jackson

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -12,8 +12,12 @@ import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.PedidoRepository;
 import com.algaworks.algafood.domain.service.EmissaoPedidoService;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -38,11 +42,24 @@ public class PedidoController {
     @Autowired
     private PedidoInputDisassembler pedidoInputDisassembler;
 
-    @GetMapping
-    public List<PedidoResumoModel> listar() {
-        List<Pedido> todosPedidos = pedidoRepository.findAll();
 
-        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+    @GetMapping
+    public MappingJacksonValue listar(@RequestParam(required = false) String filtros) {
+        List<Pedido> pedidos = pedidoRepository.findAll();
+        List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(pedidos);
+
+        MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
+
+        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+        filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+
+        if (StringUtils.isNotBlank(filtros)) {
+            filterProvider.addFilter("pedidosFilter", SimpleBeanPropertyFilter.filterOutAllExcept(filtros.split(",")));
+        }
+
+        pedidosWrapper.setFilters(filterProvider);
+
+        return pedidosWrapper;
     }
 
     @GetMapping("/{codigoPedido}")
