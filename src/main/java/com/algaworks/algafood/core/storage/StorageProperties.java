4f58c5e0045ef11204476c5452c3af639e772fbf
pertusatti22 <pertusatti22@gmail.com>
2023-07-09 15:36:58 -0300

    add 14.22. Definindo bean do client da Amazon S3 e configurando credenciais

--- src/main/java/com/algaworks/algafood/core/storage/StorageProperties.java
+++ src/main/java/com/algaworks/algafood/core/storage/StorageProperties.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.core.storage;
 
+import com.amazonaws.regions.Regions;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -29,7 +30,7 @@ public class StorageProperties {
         private String idChaveAcesso;
         private String chaveAcessoSecreta;
         private String bucked;
-        private String regiao;
+        private Regions regiao;
         private String diretorioFotos;
     }
 }
--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -1,10 +1,16 @@
 package com.algaworks.algafood.infrastructure.service.storage;
 
 import com.algaworks.algafood.domain.service.FotoStorageService;
+import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.InputStream;
 
 public class S3FotoStorageService implements FotoStorageService {
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
     @Override
     public InputStream recuperar(String nomeArquivo) {
         return null;
@@ -12,7 +18,6 @@ public class S3FotoStorageService implements FotoStorageService {
 
     @Override
     public void armazenar(NovaFoto novaFoto) {
-
     }
 
     @Override
