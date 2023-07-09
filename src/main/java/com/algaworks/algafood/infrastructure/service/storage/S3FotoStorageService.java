d0acd548070ec0a3cace8811b5104aedff73b127
pertusatti22 <pertusatti22@gmail.com>
2023-07-09 16:48:44 -0300

    add 14.24. Desafio: Implementando a exclusão de objetos do bucket da Amazon S3

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -3,7 +3,7 @@ package com.algaworks.algafood.infrastructure.service.storage;
 import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,8 +38,7 @@ public class S3FotoStorageService implements FotoStorageService {
                     storageProperties.getS3().getBucket(),
                     caminhoArquivo,
                     novaFoto.getInputStream(),
-                    objectMetadata)
-                    .withCannedAcl(CannedAccessControlList.PublicRead);
+                    objectMetadata);
 
             amazonS3.putObject(putObjectRequest);
         } catch (Exception e) {
@@ -49,7 +48,16 @@ public class S3FotoStorageService implements FotoStorageService {
 
     @Override
     public void remover(String nomeArquivo) {
+        try {
+            String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
 
+            var deleteObjectRequest = new DeleteObjectRequest(
+                    storageProperties.getS3().getBucket(), caminhoArquivo);
+
+            amazonS3.deleteObject(deleteObjectRequest);
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível excluir arquivo na Amazon S3.", e);
+        }
     }
 
     private String getCaminhoArquivo(String nomeArquivo) {
