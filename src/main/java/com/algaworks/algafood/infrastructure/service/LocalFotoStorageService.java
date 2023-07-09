bd6a34db5421cd7472603f36f35ab0b5738d7226
pertusatti22 <pertusatti22@gmail.com>
2023-07-09 09:22:46 -0300

    add 14.11. Desafio: Implementando recuperação de foto no serviço de armazenagem

--- src/main/java/com/algaworks/algafood/infrastructure/service/LocalFotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/LocalFotoStorageService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -13,6 +14,17 @@ public class LocalFotoStorageService implements FotoStorageService {
 
     @Value("${algafood.storage.local.diretorio-fotos}")
     private Path diretorioFotos;
+
+    @Override
+    public InputStream recuperar(String nomeArquivo) {
+        try {
+            Path arquivoPath = getArquivoPath(nomeArquivo);
+
+            return Files.newInputStream(arquivoPath);
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível recuperar o arquivo", e);
+        }
+    }
 
     @Override
     public void armazenar(NovaFoto novaFoto) {
