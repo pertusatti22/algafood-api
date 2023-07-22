d242213f629fbd160a1bcbd803e972e0c46ac614
pertusatti22 <pertusatti22@gmail.com>
2023-07-22 00:24:52 -0300

    add 18.6. Descrevendo informações da API na documentação

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.algaworks.algafood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,7 +19,16 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
                 .paths(PathSelectors.any())
-//          .paths(PathSelectors.ant("/restaurantes/*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Algafood API")
+                .description("Api aberta para clientes e restaurantes")
+                .version("1")
+                .contact(new Contact("Algaworks", "site algaworks", "algaworks@mail.com"))
                 .build();
     }
 }
