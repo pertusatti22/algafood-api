c508959fab4baf90adfc78636948f18cbec300b6
pertusatti22 <pertusatti22@gmail.com>
2023-08-01 22:28:13 -0300

    add 19.14. Desafio: adicionando hypermedia nos recursos de estados

--- src/main/java/com/algaworks/algafood/api/assembler/EstadoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/EstadoModelAssembler.java
@@ -1,28 +1,38 @@
 package com.algaworks.algafood.api.assembler;
 
+import com.algaworks.algafood.api.controller.EstadoController;
 import com.algaworks.algafood.api.model.EstadoModel;
 import com.algaworks.algafood.domain.model.Estado;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Component
-public class EstadoModelAssembler {
+public class EstadoModelAssembler extends RepresentationModelAssemblerSupport<Estado, EstadoModel> {
 
     @Autowired
     private ModelMapper modelMapper;
 
+    public EstadoModelAssembler() {
+        super(EstadoController.class, EstadoModel.class);
+    }
+
+    @Override
     public EstadoModel toModel(Estado estado) {
-        return modelMapper.map(estado, EstadoModel.class);
+        EstadoModel estadoModel = createModelWithId(estado.getId(), estado);
+        modelMapper.map(estado, estadoModel);
+
+        estadoModel.add(linkTo(EstadoController.class).withRel("estados"));
+
+        return estadoModel;
     }
 
-    public List<EstadoModel> toCollectionModel(List<Estado> estados) {
-        return estados.stream()
-                .map(estado -> toModel(estado))
-                .collect(Collectors.toList());
+    @Override
+    public CollectionModel<EstadoModel> toCollectionModel(Iterable<? extends Estado> entities) {
+        return super.toCollectionModel(entities).add(linkTo(EstadoController.class).withSelfRel());
     }
-
 }
--- src/main/java/com/algaworks/algafood/api/model/EstadoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/EstadoModel.java
@@ -3,7 +3,9 @@ package com.algaworks.algafood.api.model;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
+@Relation(collectionRelation = "estados")
 @Getter
 @Setter
 public class EstadoModel extends RepresentationModel<EstadoModel> {
