7e71c6c3308a1b65df3a7be07c8f5847891c8549
pertusatti22 <pertusatti22@gmail.com>
2023-08-20 09:43:42 -0300

    add 19.37. Desafio: implementando endpoint com links de recursos de estatísticas

--- src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -294,4 +294,21 @@ public class AlgaLinks {
         return linkTo(methodOn(UsuarioGrupoController.class)
                 .desassociar(usuarioId, grupoId)).withRel(rel);
     }
+
+    public Link linkToEstatisticas(String rel) {
+        return linkTo(EstatisticasController.class).withRel(rel);
+    }
+
+    public Link linkToEstatisticasVendasDiarias(String rel) {
+        TemplateVariables filtroVariables = new TemplateVariables(
+                new TemplateVariable("restauranteId", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoInicio", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoFim", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("timeOffset", TemplateVariable.VariableType.REQUEST_PARAM));
+
+        String pedidosUrl = linkTo(methodOn(EstatisticasController.class)
+                .consultarVendasDiarias(null, null)).toUri().toString();
+
+        return Link.of(UriTemplate.of(pedidosUrl, filtroVariables), rel);
+    }
 }
